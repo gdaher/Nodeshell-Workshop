7eb54069fc9b522537bdd7cab801d03cae20e3fd
rumroses12 <grace@daher.net>
2018-09-18 12:51:34 -0400

    pwd

--- bash.js
+++ bash.js
@@ -1,9 +1,16 @@
 process.stdout.write('prompt > ');
 
+
+
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
-
-    process.stdout.write('You typed: ' + cmd);
+    if(cmd === 'pwd') {
+       process.stdout.write(process.cwd());
+    } else {
+        process.stdout.write('You typed: ' + cmd);
+    }
+    
     process.stdout.write('\nprompt > ');
 
 });
+
